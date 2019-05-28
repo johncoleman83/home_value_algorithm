b18681c14ea11920786347143a00503aa7054fb9
David John Coleman II <jcoleman@pagerduty.com>
2019-05-28 00:17:14 -0500

    updated comments

--- calculate.py
+++ calculate.py
@@ -30,21 +30,23 @@ TODO: import a Big Data Library or build off of someone else's home calculator a
 
 neighborhood = "Willowbrook"
 
+
+# "elementary", "middle", "HS", "renovations" score out of 10
 SOLD_HOMES = [
-  { "sqft": 2700, "lot": 43560, "price_sold": 470000, "elementary": 7, "middle": 8, "HS": 8, "interior": 9, "year": 1988 },
-  { "sqft": 2500, "lot": 21780, "price_sold": 530000, "elementary": 10, "middle": 8, "HS": 10, "interior": 10, "year": 1957 },
-  { "sqft": 2500, "lot": 21780, "price_sold": 330000, "elementary": 9, "middle": 10, "HS": 10, "interior": 4, "year": 1978 },
-  { "sqft": 1800, "lot": 21780, "price_sold": 425000, "elementary": 9, "middle": 10, "HS": 10, "interior": 7, "year": 1960 },
-  { "sqft": 1600, "lot": 52272, "price_sold": 357000, "elementary": 8.5, "middle": 8, "HS": 10, "interior": 7, "year": 1976 },
-  { "sqft": 1400, "lot": 21780, "price_sold": 340000, "elementary": 8.5, "middle": 8, "HS": 10, "interior": 10, "year": 1978 },
-  { "sqft": 2200, "lot": 33105, "price_sold": 355000, "elementary": 7, "middle": 8, "HS": 8, "interior": 8, "year": 1975 },
-  { "sqft": 1500, "lot": 26136, "price_sold": 300000, "elementary": 10, "middle": 8, "HS": 8, "interior": 10, "year": 1964 },
-  { "sqft": 2364, "lot": 20037, "price_sold": 305000, "elementary": 10, "middle": 8, "HS": 8, "interior": 7, "year": 1956 },
-  { "sqft": 2244, "lot": 11325, "price_sold": 417000, "elementary": 8.5, "middle": 8, "HS": 10, "interior": 7, "year": 1956 },
-  { "sqft": 1849, "lot": 17424, "price_sold": 418500, "elementary": 9, "middle": 10, "HS": 10, "interior": 5, "year": 1960 },
-  { "sqft": 3000, "lot": 10890, "price_sold": 536000, "elementary": 10, "middle": 8, "HS": 10, "interior": 10, "year": 1978 },
-  { "sqft": 2250, "lot": 11325, "price_sold": 440000, "elementary": 10, "middle": 8, "HS": 10, "interior": 10, "year": 1978 },
-  { "sqft": 2800, "lot": 13068, "price_sold": 507000, "elementary": 10, "middle": 8, "HS": 19, "interior": 9, "year": 1987 }
+  { "sqft": 2700, "lot": 43560, "price_sold": 470000, "elementary": 7, "middle": 8, "HS": 8, "renovations": 9, "year": 1988 },
+  { "sqft": 2500, "lot": 21780, "price_sold": 530000, "elementary": 10, "middle": 8, "HS": 10, "renovations": 10, "year": 1957 },
+  { "sqft": 2500, "lot": 21780, "price_sold": 330000, "elementary": 9, "middle": 10, "HS": 10, "renovations": 4, "year": 1978 },
+  { "sqft": 1800, "lot": 21780, "price_sold": 425000, "elementary": 9, "middle": 10, "HS": 10, "renovations": 7, "year": 1960 },
+  { "sqft": 1600, "lot": 52272, "price_sold": 357000, "elementary": 8.5, "middle": 8, "HS": 10, "renovations": 7, "year": 1976 },
+  { "sqft": 1400, "lot": 21780, "price_sold": 340000, "elementary": 8.5, "middle": 8, "HS": 10, "renovations": 10, "year": 1978 },
+  { "sqft": 2200, "lot": 33105, "price_sold": 355000, "elementary": 7, "middle": 8, "HS": 8, "renovations": 8, "year": 1975 },
+  { "sqft": 1500, "lot": 26136, "price_sold": 300000, "elementary": 10, "middle": 8, "HS": 8, "renovations": 10, "year": 1964 },
+  { "sqft": 2364, "lot": 20037, "price_sold": 305000, "elementary": 10, "middle": 8, "HS": 8, "renovations": 7, "year": 1956 },
+  { "sqft": 2244, "lot": 11325, "price_sold": 417000, "elementary": 8.5, "middle": 8, "HS": 10, "renovations": 7, "year": 1956 },
+  { "sqft": 1849, "lot": 17424, "price_sold": 418500, "elementary": 9, "middle": 10, "HS": 10, "renovations": 5, "year": 1960 },
+  { "sqft": 3000, "lot": 10890, "price_sold": 536000, "elementary": 10, "middle": 8, "HS": 10, "renovations": 10, "year": 1978 },
+  { "sqft": 2250, "lot": 11325, "price_sold": 440000, "elementary": 10, "middle": 8, "HS": 10, "renovations": 10, "year": 1978 },
+  { "sqft": 2800, "lot": 13068, "price_sold": 507000, "elementary": 10, "middle": 8, "HS": 19, "renovations": 9, "year": 1987 }
 ]
 
 LAND_VALUE = 215000
@@ -55,7 +57,7 @@ HOME_SPEC_WEIGHT = {
   "elementary": 0.15,
   "middle": 0.15,
   "HS": 0.15,
-  "interior": 0.25,
+  "renovations": 0.25,
   "year": 0.05
 }
 
@@ -65,7 +67,7 @@ items_averages = {
     "elementary": { "list_of_values": [], "avg": 0},
     "middle": { "list_of_values": [], "avg": 0},
     "HS": { "list_of_values": [], "avg": 0},
-    "interior": { "list_of_values": [], "avg": 0},
+    "renovations": { "list_of_values": [], "avg": 0},
     "year": { "list_of_values": [], "avg": 0}
   }
 
@@ -108,12 +110,15 @@ def resolve_averages(show_output = False):
 
 def average_out(name, val, cost):
   """
-  take the costs of one home then add it to the total list of items_averages
+  input the name, amount and cost of one home item,
+  add it to the total list of items_averages
   """
   if name in ["sqft", "lot"]:
     avg_per_item = cost / val
     items_averages[name]["list_of_values"].append(avg_per_item)
-  if name in ["elementary", "middle", "HS", "interior"]:
+  if name in ["elementary", "middle", "HS", "renovations"]:
+    # this is the same calculation, the distinction is
+    # only since these values are all out of 10
     avg_per_item = cost / val
     items_averages[name]["list_of_values"].append(avg_per_item)
   if name == "year":
@@ -158,8 +163,8 @@ def execute():
   calculate_home_cost("DESIRED_HOME", DESIRED_HOME)
   calculate_home_cost("GREAT_HOME", GREAT_HOME)
 
-DESIRED_HOME = { "sqft": 2900, "lot": 1.1, "price_sold": None, "elementary": 10, "middle": 8, "HS": 10, "interior": 3, "year": 1969 }
-GREAT_HOME = { "sqft": 2900, "lot": 1.1, "price_sold": None, "elementary": 10, "middle": 10, "HS": 10, "interior": 10, "year": 1980 }
+DESIRED_HOME = { "sqft": 2900, "lot": 1.1, "price_sold": None, "elementary": 10, "middle": 8, "HS": 10, "renovations": 3, "year": 1969 }
+GREAT_HOME = { "sqft": 2900, "lot": 1.1, "price_sold": None, "elementary": 10, "middle": 10, "HS": 10, "renovations": 10, "year": 1980 }
 
 
 if __name__ == "__main__":
