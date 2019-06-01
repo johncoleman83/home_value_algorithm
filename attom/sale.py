00b02cdee0bb1006fe4950ec67e2b5cc7b86e155
David John Coleman II <jcoleman@pagerduty.com>
2019-05-31 23:45:44 -0500

    todo and readme

---
+++ attom/sale.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+ATTOM API
+https://api.developer.attomdata.com
+"""
+import requests
+from urllib.parse import quote, urlencode
+from attom import api
+from attom import addresses
+
+PATH = "assessment/snapshot"
+
+def get_sales_history():
+  """
+  TODO build this
+  """
+  return None
